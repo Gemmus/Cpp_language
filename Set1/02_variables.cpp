38ecebd6a1045e676c95a20253e3e33ed644b414
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-23 10:12:14 +0300

    Add files via upload

---
+++ Set1/02_variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main2(){
+	int counter = 0; // declaring variable "without setting a value"
+	int file_size = 100;
+	double sales = 9.99;
+
+	std::cout << file_size;
+
+	return 0;
+}
---
+++ Set1/04_constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main4(){
+
+	const double pi = 3.14;
+
+	std::cout << pi;
+
+	return 0;
+}
