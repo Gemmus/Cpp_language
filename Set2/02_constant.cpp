ffb5dd9cd7edfeceaf19eea42d4584fd697cdd72
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-24 09:33:29 +0300

    Create 02_constant.cpp

---
+++ Set2/02_constant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main2(){
+
+	const double PI = 3.14159;
+	double radius = 10;
+	double circumference = 2 * radius * PI;
+
+	std::cout << circumference << std::endl;
+
+	return 0;
+}
