7c888988dc3a28e5691f3fee091ac2b00b03d8d8
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-24 09:33:51 +0300

    Add files via upload

---
+++ Set2/01_datatype_introduction.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main1(){
+
+	int age = 21;				// integer
+	std::cout << age << std::endl;
+
+	double gpa = 4.23;			// double
+	std::cout << gpa << std::endl;
+
+	char grade = 'A'; 			// character
+	std::cout << grade << std::endl;
+
+	bool student = true; 		// boolean
+	std::cout << student << std::endl;
+
+	std::string name = "Bob";	// string
+	std::cout << name << std::endl;
+
+	return 0;
+}
