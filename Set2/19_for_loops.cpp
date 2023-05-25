59e25cce700324d2cb83f39629fe429b869cd5c3
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-25 09:20:11 +0300

    Add files via upload

---
+++ Set2/19_for_loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	for(int i = 10; i > 0; i--){
+		cout << i << endl;
+	}
+
+	cout << "Happy New Year!" << endl;
+
+	return 0;
+}
---
+++ Set2/20_break_continue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	for(int i = 0; i < 20; i++){
+		if(i == 13){
+			continue;
+		}
+		else{
+			cout << i << endl;
+		}
+	}
+
+
+	return 0;
+}
