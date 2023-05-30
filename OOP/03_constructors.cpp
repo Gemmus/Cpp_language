1def696506ef738f7da249836937a94763781a67
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-30 10:50:16 +0300

    Add files via upload

---
+++ OOP/03_constructors.cpp
@@ -0,0 +1,31 @@
+/*#include <iostream>
+
+using namespace std;
+
+class Employee{
+public:				// private, public, protected; if not stated, standard: private
+	string Name;
+	string Company;
+	int Age;
+
+	Employee(string name, string company, int age){
+		Name = name;
+		Company = company;
+		Age = age;
+	}
+
+	void Introduction(){
+		cout << "Hi! I'm " << Name << ", I work for " << Company << " and I'm " << Age << " old." << endl;
+	}
+};
+
+int main(){
+
+	Employee employee1("Chloe", "Bookworms Ltd.", 36);
+	Employee employee2("Mona", "Flower Delivered Ltd.", 23);
+	employee1.Introduction();
+	employee2.Introduction();
+
+	return 0;
+}
+*/
