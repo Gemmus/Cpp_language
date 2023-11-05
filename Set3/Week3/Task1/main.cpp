c12b22a5c9c4ff24da40cc0c159a62e20d5a178e
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-11-05 16:24:26 +0200

    Create main.cpp

---
+++ Set3/Week3/Task1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int *allocate_table(int n);
+
+int main() {
+
+    int array_size = 0;
+
+    cout << "Enter the number of array elements: ";
+    cin >> array_size;
+
+    int *array_ptr = allocate_table(array_size);
+
+    if (array_ptr != nullptr) {
+        cout << "The memory address of first array element: " << array_ptr << endl;
+    } else {
+        cout << "The array initiation was unsuccessful." << endl;
+    }
+
+   return 0;
+}
+
+int *allocate_table(int n) {
+
+    if (n <= 0) {
+        return nullptr;
+    }
+
+    int *array;
+    array = new int [n];
+
+    return &array[0];
+}
+
