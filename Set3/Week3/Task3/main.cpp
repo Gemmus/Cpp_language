a536f0efc9c45ea179e52c56a6d6a2d8b0e39b3b
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-11-07 18:14:30 +0200

    Update main.cpp

--- Set3/Week3/Task3/main.cpp
+++ Set3/Week3/Task3/main.cpp
@@ -12,7 +12,7 @@ void reverse(int *t, int n);
 
 int main() {
   
-  int task3_array[] = {0,1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int task3_array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
   int task3_arr_size = sizeof(task3_array)/sizeof(task3_array[0]);
 
   cout << "Original array: ";
