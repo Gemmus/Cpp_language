3d2c6a1cd9a0af9416c936be4b352aceabfdfdd1
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-11-15 20:37:55 +0200

    Create main.cpp

---
+++ Set3/Week4/Task3/main.cpp
@@ -0,0 +1,50 @@
+/*
+Fix the problem using reference parameter in the function f instead of value parameter.
+Why this very small modification fixes the problem?
+*/
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class String{
+
+public:
+
+    String(const char* string) {
+        c_string = new char[strlen(string) + 1];
+        strcpy(c_string, string);
+
+    }
+
+    ~String() {
+        delete c_string;
+        cout << "destructor done" << endl;
+    }
+
+    void list() {
+        cout << c_string << endl;
+    }
+
+private:
+
+    char *c_string;
+
+};
+
+void f(String &s);
+
+int main() {
+
+    String s("abcdefg");
+    s.list();
+    f(s);
+    s.list();
+
+    return 0;
+}
+
+void f(String &s) {
+    s.list();
+}
