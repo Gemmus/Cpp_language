6efe63deaaf843ef7470e889e4bc9388c68a14d9
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-11-15 19:27:47 +0200

    Update main.cpp

--- Set3/Week4/Task1/main.cpp
+++ Set3/Week4/Task1/main.cpp
@@ -1,17 +1,21 @@
 /*
 Write the class string that has only one data member c_string. This data member represents
 a string which is similar in C-language.
+
 In this phase 1 we assume that the maximum number of characters of the string is 10. That's
 why we can declare the string (the character array) inside the class definition as follows
 char c_string[11];
+
 In this phase 1 you should do the following member functions for the class String1:
 • constructor that takes the string to be stored in String object as a parameter.
 • list, that displays the string on the screen
-        In addition to these two member functions, write the third function (ordinary function, not
+
+In addition to these two member functions, write the third function (ordinary function, not
 a member of the class) f that takes String object as a parameter and only displays the
-        contents of the string to the screen using String class member function list. The prototype of
-        the function is
+contents of the string to the screen using String class member function list. The prototype of
+the function is:
 void f(String s);
+
 Test the class String using the following test program:
 void main() {
     String s("abcdefg");
@@ -19,6 +23,7 @@ void main() {
     f(s);
     s.list();
 }
+
 Examine the outputs from the program and note that all of them are correct.
 */
 
