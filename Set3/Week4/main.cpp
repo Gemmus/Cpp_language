954aea5e949fcf3cb5739dee189dfdfb39ab56ef
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-11-18 16:16:25 +0200

    Correction of explanations.

--- Set3/Week4/main.cpp
+++ Set3/Week4/main.cpp
@@ -12,9 +12,9 @@
 
 using namespace std;
 
-                    ////////////////////////////////////////////
-                    //               TASK 1                   //
-                    ////////////////////////////////////////////
+                                               ////////////////////////////////////////////
+                                               //                TASK 1                  //
+                                               ////////////////////////////////////////////
 #ifdef TASK1
 
 class String{
@@ -61,9 +61,9 @@ void f(String s) {
 
 #endif
 
-                    ////////////////////////////////////////////
-                    //               TASK 2                   //
-                    ////////////////////////////////////////////
+                                                ////////////////////////////////////////////
+                                                //                TASK 2                  //
+                                                ////////////////////////////////////////////
 #ifdef TASK2
 
 class String{
@@ -74,7 +74,7 @@ public:
         c_string = new char[strlen(string) + 1];
         strcpy(c_string, string);
     }
-
+    
     ~String() {
         delete c_string;
         cout << "destructor done" << endl;
@@ -117,14 +117,16 @@ void f(String s) {
    _____________________________________________________________________________________________________________________________________________
      Explanation:
    ---------------------------------------------------------------------------------------------------------------------------------------------
-     Because the function is creating a copy of the parameter "String s", which appears as a new, second object to the first, original one.
+     Because in the class there is no special copy constructor that would create a dynamic copy of the value parameter. After the function, the 
+     destructor is called that destroys the original content where it was pointed to, thus the programme is printing garbage and then tries to 
+     release the same memory for the second time which leads to the crash of the programme. 
    _____________________________________________________________________________________________________________________________________________ */
 
 #endif
 
-                    ////////////////////////////////////////////
-                    //               TASK 3                   //
-                    ////////////////////////////////////////////
+                                                ////////////////////////////////////////////
+                                                //                TASK 3                  //
+                                                ////////////////////////////////////////////
 #ifdef TASK3
 
 class String{
@@ -177,14 +179,14 @@ void f(String &s) {
    _____________________________________________________________________________________________________________________________________________
      Explanation:
    ---------------------------------------------------------------------------------------------------------------------------------------------
-     Because now it is referring to the memory address and no new copy is created by function f().
+     Because now it is referring to the memory address which is not destroyed after the function call.
    _____________________________________________________________________________________________________________________________________________ */
 
 #endif
 
-                ////////////////////////////////////////////
-                //               TASK 4                   //
-                ////////////////////////////////////////////
+                                                ////////////////////////////////////////////
+                                                //                TASK 4                  //
+                                                ////////////////////////////////////////////
 #ifdef TASK4
 
 class String {
@@ -237,19 +239,20 @@ void f(String s) {
    ---------------------------------------------------------------------------------------------------------------------------------------------
      abcdefg
      abcdefg
+     destructor done
      abcdefg
      destructor done
    _____________________________________________________________________________________________________________________________________________
      Explanation:
    ---------------------------------------------------------------------------------------------------------------------------------------------
-     Copy constructor is making a copy of the original object.
+     Copy constructor is making a copy of the original object dynamically and only the copy is destroyer at the end of the function call.
    _____________________________________________________________________________________________________________________________________________ */
 
 #endif
 
-                ////////////////////////////////////////////
-                //          EXTRA TASK /TASK 5            //
-                ////////////////////////////////////////////
+                                                ////////////////////////////////////////////
+                                                //          EXTRA TASK /TASK 5            //
+                                                ////////////////////////////////////////////
 #ifdef TASK5
 
 class String {
@@ -306,27 +309,7 @@ int main() {
      memory allocation, such as memory management bookkeeping.
 
      Example output:
-     0 bytes in 0 Free Blocks.
-     27 bytes in 1 Normal Blocks.
-     22767 bytes in 106 CRT Blocks.
-     0 bytes in 0 Ignore Blocks.
-     0 bytes in 0 Client Blocks.
-     Largest number used: 22794 bytes.
-     Total allocations: 49208 bytes.
-     0 bytes in 0 Free Blocks.
-     54 bytes in 2 Normal Blocks.
-     22767 bytes in 106 CRT Blocks.
-     0 bytes in 0 Ignore Blocks.
-     0 bytes in 0 Client Blocks.
-     Largest number used: 22821 bytes.
-     Total allocations: 49235 bytes.
-     0 bytes in 0 Free Blocks.
-     81 bytes in 3 Normal Blocks.
-     22767 bytes in 106 CRT Blocks.
-     0 bytes in 0 Ignore Blocks.
-     0 bytes in 0 Client Blocks.
-     Largest number used: 22848 bytes.
-     Total allocations: 49262 bytes.
+     ---------------
      Detected memory leaks!
      Dumping objects ->
      {159} normal block at 0x00000153B657CBD0, 27 bytes long.
@@ -339,32 +322,8 @@ int main() {
    ____________________________________________________________________________________________________________________________________________
      Why the destructor solves the memory leak problem? Why in this case you need to write a special destructor?
    ---------------------------------------------------------------------------------------------------------------------------------------------
-     Destructor frees the memory blocks that were allocated dynamically, thus preventing possible memory leakages. I didn't create a special
-     destructor, only restored the original one from the previous task. Tested it multiple times, however, _CrtDumpMemoryLeaks() did not report
-     memory leakage in any of the cases.
-
-     Example output:
-     0 bytes in 0 Free Blocks.
-     0 bytes in 0 Normal Blocks.
-     22821 bytes in 108 CRT Blocks.
-     0 bytes in 0 Ignore Blocks.
-     0 bytes in 0 Client Blocks.
-     Largest number used: 22848 bytes.
-     Total allocations: 49300 bytes.
-     0 bytes in 0 Free Blocks.
-     0 bytes in 0 Normal Blocks.
-     22821 bytes in 108 CRT Blocks.
-     0 bytes in 0 Ignore Blocks.
-     0 bytes in 0 Client Blocks.
-     Largest number used: 22848 bytes.
-     Total allocations: 49327 bytes.
-     0 bytes in 0 Free Blocks.
-     0 bytes in 0 Normal Blocks.
-     22821 bytes in 108 CRT Blocks.
-     0 bytes in 0 Ignore Blocks.
-     0 bytes in 0 Client Blocks.
-     Largest number used: 22848 bytes.
-     Total allocations: 49354 bytes.
+     Destructor frees the memory blocks that were allocated dynamically, thus preventing possible memory leakages. A special destructor needs to
+     be implemeted if dynamic memory allocation is used.
    _____________________________________________________________________________________________________________________________________________ */
 
 #endif
