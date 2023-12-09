d285cd4106290a8dd2ba3c0af7f0a43bc5098a40
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-09 15:56:30 +0200

    Create main.cpp

---
+++ Set3/Week7/main.cpp
@@ -0,0 +1,137 @@
+#include "counter.h"
+#include <iostream>
+
+/////////////////////////// JARKKO ///////////////////////////
+#define TASK7B // Define selected task (TASK7A, TASK7B, TASKE)
+//////////////////////////////////////////////////////////////
+
+using namespace std;
+
+/**************************
+ *        TASK  7A        *
+ **************************/
+
+#ifdef TASK7A
+
+class LimitedCounter {
+    friend ostream& operator<<(ostream& output, const LimitedCounter& c1) {
+        output << "" << c1.counter << "";
+        return output;
+    }
+
+public:
+    LimitedCounter(int n0=0, int n1=100) {
+        counter = n0;
+        upper_limit = n1;
+    }
+
+    LimitedCounter operator++(int) {
+        LimitedCounter old = *this;
+        if (old < upper_limit) {
+            counter++;
+        }
+        return old;
+    }
+
+    LimitedCounter& operator++() {
+        if (counter < upper_limit) {
+            ++counter;
+        }
+        return *this;
+    }
+
+    void reset() {
+        counter.reset();
+    }
+
+    int getCount() const {
+        return counter.getCount();
+    }
+
+    bool operator<(int c2) const {
+        return (counter < c2);
+    }
+
+private:
+    Counter counter;
+    int upper_limit;
+};
+
+int main() {
+    LimitedCounter lc(0, 5); //initial value 0, upper limit 5
+    cout << lc++ << endl; //output should be 0
+    cout << ++lc << endl; //output should be 2
+    lc.reset();
+    for (int i = 0; i < 9; i++) {
+        lc++;
+        cout << lc << endl; //output is 1 2 3 4 5 5 5 5 5
+    }
+    cout << "\nlc.getCount(): " << lc.getCount() << endl; //output is 5
+    cout << "\n(lc < 7): " << (lc < 7) << endl; //Testing the comparison operator //output is 1
+    cout << "\n(lc < 1): " << (lc < 1) << endl; //Testing the comparison operator //output is 0
+    return 0;
+}
+
+#endif
+
+/**************************
+ *        TASK  7B        *
+ **************************/
+
+#ifdef TASK7B
+
+class LimitedCounter: public Counter {
+public:
+    explicit LimitedCounter(int n0=0, int n1=100):Counter(n0) {
+        upper_limit = n1;
+    }
+
+    LimitedCounter &operator++() {
+        if (getCount() < upper_limit) {
+            Counter::operator++();
+        }
+        return *this;
+    }
+
+    LimitedCounter operator++(int) {
+        LimitedCounter old = *this;
+        if (getCount() < upper_limit) {
+            Counter::operator++(0);
+        }
+        return old;
+    }
+
+    bool operator<(int value) const {
+        return getCount() < value;
+    }
+
+private:
+    int upper_limit;
+};
+
+int main() {
+    LimitedCounter lc(3, 5); //initial value 3, upper limit 5
+    cout << lc++ << endl; //output should be 3
+    cout << ++lc << endl; //output should be 5
+    lc.reset();
+    for (int i = 0; i < 9; i++) {
+        ++lc;
+        cout << lc << endl; //output is 1 2 3 4 5 5 5 5 5
+    }
+    cout << "\nlc.getCount(): " << lc.getCount() << endl; //output is 5
+    cout << "\n(lc < 7): " << (lc < 7) << endl; //Testing the comparison operator //output is 1
+    cout << "\n(lc < 1): " << (lc < 1) << endl; //Testing the comparison operator //output is 0
+    return 0;
+}
+
+#endif
+
+/**************************
+ *        TASK  E         *
+ **************************/
+
+#ifdef TASKE
+
+
+
+#endif
