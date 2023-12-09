47f0ec74508b467adec8f3e7a4a694b8e413f117
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-09 17:40:21 +0200

    Update main.cpp

--- Set3/Week7/main.cpp
+++ Set3/Week7/main.cpp
@@ -1,9 +1,9 @@
 #include "counter.h"
 #include <iostream>
 
-/////////////////////////// JARKKO ///////////////////////////
-#define TASK7B // Define selected task (TASK7A, TASK7B, TASKE)
-//////////////////////////////////////////////////////////////
+/////////////////////////// JARKKO ////////////////////////////
+#define TASK7E // Define selected task (TASK7A, TASK7B, TASK7E)
+///////////////////////////////////////////////////////////////
 
 using namespace std;
 
@@ -123,11 +123,78 @@ int main() {
 #endif
 
 /**************************
- *        TASK  E         *
+ *        TASK  7E        *
  **************************/
 
-#ifdef TASKE
+#ifdef TASK7E
 
+class LimitedCounter {
 
+friend ostream& operator<<(ostream& output, const LimitedCounter& c1) {
+    output << "\nCounter value is now " << c1.countPtr->getCount() ;
+    return output;
+}
+
+public:
+    LimitedCounter(int n0=0, int n1=100) {
+        countPtr = new Counter(n0);
+        upper_limit = n1;
+    }
+
+    LimitedCounter(const LimitedCounter &cp) {
+        countPtr = new Counter(*cp.countPtr);
+        upper_limit = cp.upper_limit;
+    }
+
+    ~LimitedCounter(){
+        delete countPtr;
+    }
+
+    LimitedCounter operator++(int) {
+        LimitedCounter old = *this;
+        if (countPtr->getCount() < upper_limit) {
+            (*countPtr)++;
+        }
+        return old;
+    }
+
+    LimitedCounter& operator++() {
+        if (countPtr->getCount() < upper_limit) {
+            ++(*countPtr);
+        }
+        return *this;
+    }
+
+    void reset() {
+        countPtr->reset();
+    }
+
+    int getCount() const {
+        return countPtr->getCount();
+    }
+
+    bool operator<(int c2) const {
+        return (countPtr->getCount() < c2);
+    }
+
+private:
+    Counter *countPtr;
+    int upper_limit;
+};
+
+int main() {
+    LimitedCounter lc(3, 5); //initial value 3, upper limit 5
+    cout << lc++ << endl; //output should be 3
+    cout << ++lc << endl; //output should be 5
+    lc.reset();
+    for (int i = 0; i < 9; i++) {
+        ++lc;
+        cout << lc << endl; //output is 1 2 3 4 5 5 5 5 5
+    }
+    cout << "\nlc.getCount(): " << lc.getCount() << endl; //output is 5
+    cout << "\n(lc < 7): " << (lc < 7) << endl; //Testing the comparison operator //output is 1
+    cout << "\n(lc < 1): " << (lc < 1) << endl; //Testing the comparison operator //output is 0
+    return 0;
+}
 
 #endif
